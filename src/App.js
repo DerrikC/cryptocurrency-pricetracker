b872231ccde39c6eb1c299846761116ef524def5
Derrik Campbell <derrikcampbell@hotmail.com>
2021-12-21 17:30:33 -0500

    Added updates for red and green colour changes on crypto price and pulled more data

--- src/App.js
+++ src/App.js
@@ -44,8 +44,10 @@ const filteredCoins = coins.filter(coin =>
         name={coin.name} 
         image={coin.image}
         symbol={coin.symbol}
-        volume={coin.market_cap}
+        marketcap={coin.market_cap}
         price={coin.current_price}
+        priceChange={coin.price_change_percentage_24h}
+        volume={coin.total_volume}
         />
       ) 
     })}
